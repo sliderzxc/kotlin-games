c019961d3d645f83fb9278b3bf40fa11cc1170ce
vadwwxz <vadwwxz@gmail.com>
2023-10-10 22:06:45 +0300

    Initial commit

---
+++ core/src/main/kotlin/com/sliderzxc/games/core/MyClass.kt
@@ -0,0 +1,5 @@
+package com.sliderzxc.games.core
+
+fun main() {
+    println("Hello kotlin-console-games")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+rootProject.name = "kotlin-console-games"
+include(":core")
